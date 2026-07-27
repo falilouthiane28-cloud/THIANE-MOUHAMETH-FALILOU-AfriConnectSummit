26c90be5708d0da368fb460d355ff8eb6144cec1
Fallou thiane <falilouthiane28@gmail.com>
2026-07-27 16:31:47 +0000

    JS: filtages du onglets programme , filtrages des intervenats ,Anne dynamique du footer et validations du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -6,10 +6,8 @@ btnMobile.addEventListener('click', () =>{
 });
 
 // BUTTON MODE JOURS/NUITS
-
 const toggleBtn = document.getElementById('theme-toggle');
 const icon = toggleBtn.querySelector('i');
-
 if(localStorage.getItem('theme')==='dark'){
     document.body.classList.add('dark-mode');
     toggleBtn.textContent = '🔆';
@@ -26,7 +24,6 @@ toggleBtn.addEventListener('click',()=> {
 }  
 );
 //  NAVBAR INTERACIVE AU SCROLL
-
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll',() => {
     if (window.scrollY >80){
@@ -85,11 +82,10 @@ setInterval(() => {
     const heures = Math.floor((temps / 3600000) % 24);
     const minutes = Math.floor((temps / 60000) % 60);
     const secondes = Math.floor((temps / 1000) % 60);
-
-    document.getElementById("jours").textContent = jours;
-    document.getElementById("heures").textContent = heures;
-    document.getElementById("minutes").textContent = minutes;
-    document.getElementById("second").textContent = secondes;
+ const joursElement=   document.getElementById("jours").textContent = jours;
+ const heuresElement =   document.getElementById("heures").textContent = heures;
+ const minutesElement =  document.getElementById("minutes").textContent = minutes;
+ const secondesElement =   document.getElementById("second").textContent = secondes;
 }, 1000);
 
 // ANIMATIONS FADE-IN , SLIDE-LEFT, ZOOM-IN 
@@ -111,4 +107,104 @@ if (animatedElements.length > 0) {
     animatedElements.forEach((element) => {
         animationObserver.observe(element);
     });
+}
+// FILTRAGE ONGLETS PROGRAMME
+const bouttons = document.querySelectorAll('.btn-tab');
+const contenus = document.querySelectorAll('.tab-container');
+bouttons.forEach((boutton) => {
+    boutton.addEventListener('click',() => {
+        bouttons.forEach((btn) => {
+            btn.classList.remove('active');
+        } );
+        contenus.forEach((contenu) => {
+            contenu.classList.remove('active');
+        });
+        boutton.classList.add('active');
+        document.getElementById(boutton.dataset.tab)
+        .classList.add('active')
+    } );
+});
+// FILTRAGE INTERVENANTS
+const filterButtons = document.querySelectorAll('.filter-btn');
+const persoCards = document.querySelectorAll('.sec-card');
+filterButtons.forEach((boutton) => {
+    boutton.addEventListener('click',() => {
+        filterButtons.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+        boutton.classList.add('active');
+        const filter = boutton.dataset.filter;
+        persoCards.forEach((card) => {
+        const categori = card.dataset.categori;
+if (filter === 'Tous' || categori === filter){
+    card.classList.remove('non');
+}else  {
+    card.classList.add('non');
+}
+        });
+
+    });
+} );
+
+// VALIDATIONS FORMULAIRE
+const form = document.querySelector("form");
+if(form){ 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let valide = true;
+    const fullname = document.getElementById("fullname");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    const participation = document.getElementById("participations");
+    const pays = document.getElementById("pays");
+    const message = document.getElementById("message");
+
+    document.querySelectorAll(".erreur-message").forEach(function (erreur) {
+        erreur.textContent = "";
+    });
+
+    if (fullname.value.trim() === "") {
+        fullname.nextElementSibling.textContent = "Le nom est obligatoire.";
+        valide = false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email.value.trim())) {
+        email.nextElementSibling.textContent = "Adresse e-mail invalide.";
+        valide = false;
+    }
+
+    const chiffres = phone.value.replace(/\D/g, "");
+
+    if (chiffres.length < 8) {
+        phone.nextElementSibling.textContent =
+            "Le téléphone doit contenir au moins 8 chiffres.";
+        valide = false;
+    }
+    if (participation.value === "") {
+        participation.nextElementSibling.textContent =
+            "Choisissez un type de participation.";
+        valide = false;
+    }
+    if (pays.value === "") {
+        pays.nextElementSibling.textContent =
+            "Sélectionnez un pays.";
+        valide = false;
+    }
+    if (message.value.trim().length < 20) {
+        message.nextElementSibling.textContent =
+            "La motivation doit contenir au moins 20 caractères.";
+        valide = false;
+    }
+    if (valide) {
+        alert("Votre demande a été envoyée avec succès.");
+        form.reset();
+    }
+});
+}
+// ANNNE DYNAMIQUE DU FOOTER
+const year = document.getElementById('year');
+if (year){
+    year.textContent= new Date().getFullYear();
 }
